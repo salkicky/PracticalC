b58c38d7c3f1282fd472d95f7488f04001c2bdf0
hamada <aa@gmail.com>
2013-06-05 12:57:44 +0900

    add 06/ original total.c

---
+++ 06/total/total.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/*******************************
+ * 仕様：
+ *      入力された数値を足し続ける。
+ *      '0'を入力されたら抜ける。
+ *******************************/
+int main()
+{
+    char line[80];
+    int total = 0;
+    int  input_num = 0;
+    int ret = 0;
+
+    while(1) {
+        /* 入力数値の初期化 */
+        input_num  = 0;
+
+        /* 数値入力 */
+        printf("please input number : ");
+        fgets(line, sizeof(line), stdin);
+        ret = sscanf(line, "%d", &input_num);
+
+        /* 判定 */
+        if (ret == 0) {
+            /* 入力された文字が数字でなければ */
+            printf("[ERROR] you input wrong number! please retry.\n");
+        } else if(input_num == 0) {
+            /* 0 を入力で抜ける */
+            printf("bye\n");
+            break;
+        } else {
+            /* ここまで正常なら加算 */
+            total += input_num;
+        }
+        printf("  total = %d\n", total);
+    }
+
+    return (0);
+}
