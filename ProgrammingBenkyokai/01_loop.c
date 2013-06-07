79d19c3017504d25f08e0ba79c2a0504c9da1d10
hamada <aa@gmail.com>
2013-06-07 17:30:41 +0900

    rename

--- ProgrammingBenkyokai/01_loop.c
+++ ProgrammingBenkyokai/01_loop.c
@@ -14,10 +14,20 @@ int main()
     int stairs_num;
     int astah_num;
 
+    int cnt;
+
     astah_num = 1;
 
     for(stairs_num = 5; 0 < stairs_num; stairs_num--) {
-
+        for (cnt = 0; cnt < stairs_num; cnt++) {
+            printf(" ");
+        }
+        for (cnt = 0; cnt < astah_num; cnt++) {
+            printf("*");
+        }
+        printf("\n");
+        astah_num += 2;
     }
+    return 0;
 
 }
