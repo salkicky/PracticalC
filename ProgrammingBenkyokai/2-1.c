e8c7bb38767de986e50198f200bd179bd8db7cd9
hamada <aa@gmail.com>
2013-06-12 12:52:30 +0900

    2-1 complete.

--- ProgrammingBenkyokai/2-1.c
+++ ProgrammingBenkyokai/2-1.c
@@ -6,26 +6,32 @@
 int main()
 {
     char line[80];
+    int max_check_num;
     int check_num;
     int prime_counter;
 
     int i;
 
     /* チェック数列を入力 */
-    printf("Please input check number > ");
+    printf("Please input number > ");
     fgets(line, sizeof(line), stdin);
-    sscanf(line, "%d", &check_num);
+    sscanf(line, "%d", &max_check_num);
 
     prime_counter = 0;
 
-    for (i = 2; i < check_num; i++) {
-        if ((check_num % i) == 0) {
-            break;
+    /* 入力された数値まで順番に素数を数える */
+    for (check_num = 1; check_num < max_check_num; check_num++) {
+        /* check_numが素数かどうか？の判定 */
+        for (i = 2; i < check_num; i++) {
+            if ((check_num % i) == 0) {
+                break;
+            }
         }
-    }
-
-    if (i == check_num) {
-        prime_counter++;
+        
+        /* 素数ならカウント */
+        if (i == check_num) {
+            prime_counter++;
+        }
     }
 
     printf("prime count : %d\n", prime_counter); 
