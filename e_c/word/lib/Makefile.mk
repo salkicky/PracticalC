ce4b685161d7943fc6109aaafe6d1e984f99298e
salkicky <salkicky@gmail.com>
2014-03-03 23:17:43 +0900

    makefile

--- e_c/word/lib/Makefile.mk
+++
@@ -1,7 +0,0 @@
-all:
-	make -i -f util/Makefile.mk all
-	make -i -f dict/Makefile.mk all
-
-clean:
-	make -i -f util/Makefile.mk clean
-	make -i -f dict/Makefile.mk clean
--- e_c/word/main_get_word.c
+++ e_c/word/main_get_word.c
@@ -5,6 +5,10 @@ int main(void)
 {
     char word_buf[256];
 
+    printf("This program separates words from sentence.\n");
+    printf("Please input sentence >\n");
+    fflush(stdout);
+
     while (get_word(word_buf, 256, stdin) != -1) {
         printf("<<%s>>\n", word_buf);
         fflush(stdout);
